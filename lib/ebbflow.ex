892fae89b4e51c5b58b0c707a41b2697ff260ab5
Yajur Ahuja <yajurahuja@Yajurs-MacBook-Pro.local>
2022-12-04 18:11:39 -0500

    init

---
+++ lib/ebbflow.ex
@@ -0,0 +1,18 @@
+defmodule Ebbflow do
+  @moduledoc """
+  Documentation for `Ebbflow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ebbflow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ebbflow_test.exs
@@ -0,0 +1,8 @@
+defmodule EbbflowTest do
+  use ExUnit.Case
+  doctest Ebbflow
+
+  test "greets the world" do
+    assert Ebbflow.hello() == :world
+  end
+end
