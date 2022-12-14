7410e355a7420569ac4ab74550a8a916a92ba723
culechetoo <chaitanya16027@iiitd.ac.in>
2022-12-14 04:37:34 -0500

    elja

--- lib/streamlet.ex
+++ lib/streamlet.ex
@@ -97,7 +97,6 @@ defmodule PClient do
 
   @spec isnotarized(%PClient{}, %PBlock{}, non_neg_integer()) :: boolean()
   def isnotarized(client, block, n) do
-    IO.puts(inspect(client.votes[block]))
     cond do
       #if the current block is the genesis block: return true
       block == client.global_genesis_block -> true
@@ -279,7 +278,7 @@ defmodule PClient do
       msg = hd(msgs_in)
       if pMsgVote?(msg) do
         #IO.puts("MsgVote!")
-        new_votes = MapSet.put(client.votes[msg.block], msg.id)
+        new_votes = MapSet.put(Map.get(client.votes, msg.block, MapSet.new()), msg.id)
         updated_votes = Map.put(client.votes, msg.block, new_votes)
         client = %{client | votes: updated_votes}
         slot_vote_helper(client, tl(msgs_in))
